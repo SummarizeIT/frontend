9d1ba6606377aff0f4e41c2034831294e19c6796
AbdAlwahabAbuKhalil <100605138+AbdAlwahabAbuKhalil@users.noreply.github.com>
2024-03-30 23:25:36 +0300

    Add sign in page & sign up page

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
+import { ThemeProvider } from "@/components/theme-provider.tsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import LandingPage from "./landingpage.tsx";
-import { ThemeProvider } from "@/components/theme-provider.tsx";
+import SignUpPage from "./pages/auth/signUp.tsx";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
-      <LandingPage />
+      <SignUpPage />
     </ThemeProvider>
   </React.StrictMode>
 );

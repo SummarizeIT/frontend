54d848a28da4462c0e5e2b0a8ce5b15fd8154c58
Mohammad Khawaja <104163142+AL-Khawaja@users.noreply.github.com>
2024-05-13 08:47:24 +0300

    edit publicview page

--- src/components/mediaPage/publicview.tsx
+++ src/components/mediaPage/publicview.tsx
@@ -3,35 +3,40 @@ import CssBaseline from '@mui/joy/CssBaseline';
 import Stack from '@mui/joy/Stack';
 import { CssVarsProvider } from '@mui/joy/styles';
 import React, { useCallback, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import MediaPlayer from './parts/mediaPlayer';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import { EntryService } from '@/client';
 import NavBar from '@/components/fileview/components/NavBar';
+import { Tab, TabList, TabPanel, Tabs } from '@mui/joy';
+import MDEditor from '@uiw/react-md-editor';
+import ReactPlayer from 'react-player';
 interface MediaPageProps {
   mediaUrl?: string;
   transcription?: string;
   description?: string;
 }
 
-const PublicView: React.FC<MediaPageProps> = ({
-}) => {
+const PublicView: React.FC<MediaPageProps> = ({}) => {
+  const navigate = useNavigate();
   const { id } = useParams<{ id: string }>();
   const [url, setUrl] = useState<string | null>(null);
+  const [transcription, setTranscription] = useState<string | null>(null);
+  const [summarization, setSummarization] = useState<string | null>(null);
 
-
-  const getEntryDetails = useCallback(
-    () => {
-      if (!id) return;
-      EntryService.getEntryById({ id: id }).then((response) => {
-        console.log(response);
+  const getEntryDetails = useCallback(() => {
+    if (!id) return;
+    EntryService.getEntryById({ id: id })
+      .then((response) => {
         setUrl(response.url);
-      }).catch((error) => {
+        setTranscription(response.transcript);
+        const bodyContent = response.extensions.find(ext => ext.identifier === "body")?.content?.text ?? undefined;
+        setSummarization(bodyContent as string);
+      })
+      .catch((error) => {
         console.error(error);
+        navigate("/404");
       });
-    },
-    [id]
-  );
+  }, [id]);
 
   useEffect(() => {
     if (id) {
@@ -39,14 +44,76 @@ const PublicView: React.FC<MediaPageProps> = ({
     }
   }, [getEntryDetails, id]);
 
+  const tabs = [];
+  const tabPanels = [];
+
+  tabs.push(<Tab key="Transcription">Transcription</Tab>);
+  tabPanels.push(
+    <TabPanel key="Transcription" value={0}>
+      <MDEditor.Markdown
+        source={transcription || ""}
+        style={{
+          whiteSpace: "pre-wrap",
+          direction: "rtl",
+          backgroundColor: "transparent",
+        }}
+      />
+    </TabPanel>
+  );
+
+  tabs.push(<Tab key="Summarization">Summarization</Tab>);
+  tabPanels.push(
+    <TabPanel key="Summarization" value={1}>
+      <MDEditor.Markdown
+        source={summarization || ""}
+        style={{
+          backgroundColor: "transparent",
+        }}
+      />
+    </TabPanel>
+  );
+
+
+ 
   return (
+
     <CssVarsProvider disableTransitionOnChange>
       <CssBaseline />
-      <NavBar/>
-      <Box component="main" sx={{ height: 'calc(100vh - 55px)', display: 'grid', gridTemplateColumns: { xs: 'auto', md: '60% 40%' }, gridTemplateRows: 'auto 1fr auto', }}>
+      <NavBar />
+      <Box
+        component="main"
+        sx={{ height: "calc(100vh - 49px)", display: "grid" }}
+      >
+        <Stack
+          sx={{
+            backgroundColor: "background.surface",
+            px: { xs: 2, md: 4 },
+            py: 2,
+            borderBottom: "1px solid",
+            borderColor: "divider",
+          }}
+        >
+          <ReactPlayer width="100%" height="60%" url={url!} controls />
+          <Tabs
+            aria-label="Sticky tabs"
+            sx={{
+              p: 1,
+              height: 270,
+              overflowY: "auto",
+              width: "100%",
+            }}
+          >
+            <TabList sticky={"top"} variant="soft">
+              {tabs}
+            </TabList>
+            {tabPanels}
+          </Tabs>
+        </Stack>
       </Box>
     </CssVarsProvider>
+
   );
+
 };
 
 export default PublicView;
